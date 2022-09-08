648c02c9d5b83019c75c1086ecc73cd576b1c546
ci3l4kM4sl4k <dubieltomasz@protonmail.com>
2022-09-08 18:44:35 +0200

    Update bubbleSorting.cpp

--- bubbleSorting.cpp
+++ bubbleSorting.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
 using namespace std;
 
+///////////////////////
+/*void bubbleSorting(int arr[], int n)
+{
 
+}*/
+///////////////////////
 
 int main()
 {
-    
+    int arr[]={9,8,7,6,5,4,3,2,1};
+    int n= sizeof(arr) / sizeof(arr[0]);
 
+    cout << n <<"\n";
+
+    for(int i=0; i<n; i++)
+        cout << arr[i] << "\t";
+
+    bubbleSorting
+
+      for(int i=0; i<n; i++)
+        cout << arr[i] << "\t";  
 
     return 0;
 }
