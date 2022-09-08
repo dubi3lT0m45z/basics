647d3eaca94b538c34c878db81ca4c5230147812
ci3l4kM4sl4k <dubieltomasz@protonmail.com>
2022-09-08 19:10:14 +0200

    Update bubbleSorting.cpp

--- bubbleSorting.cpp
+++ bubbleSorting.cpp
@@ -1,27 +1,35 @@
+// Bubble sort for c++ language
 #include <iostream>
 using namespace std;
 
-///////////////////////
-/*void bubbleSorting(int arr[], int n)
+/////////////////////// sorting function
+void bubbleSort(int arr[], int n)
 {
-
-}*/
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            if (arr[j] > arr[j + 1])
+            {
+                int tmp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = tmp;
+            }
+}
 ///////////////////////
 
 int main()
 {
-    int arr[]={9,8,7,6,5,4,3,2,1};
-    int n= sizeof(arr) / sizeof(arr[0]);
+    int n = 9; //size of array
+    int arr[]={9,8,7,6,5,4,3,2,1}; //writing values to array
 
-    cout << n <<"\n";
+    for(int i=0; i<n ; i++)
+        cout << arr[i] << "\t"; //reads unsorted array
+    cout << endl;
 
-    for(int i=0; i<n; i++)
-        cout << arr[i] << "\t";
+    bubbleSort(arr, n); //using bubblesort algorithm
 
-    bubbleSorting
-
-      for(int i=0; i<n; i++)
-        cout << arr[i] << "\t";  
+    for(int i=0; i<n ; i++)
+        cout << arr[i] << "\t"; //reads sorted array
+    cout << endl;
 
     return 0;
 }
