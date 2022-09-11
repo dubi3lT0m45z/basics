a08929e59a593ada76bdbadd08a2233c077bde36
ci3l4kM4sl4k <dubieltomasz@protonmail.com>
2022-09-11 13:01:37 +0200

    Update primeFactors.cpp

--- primeFactors.cpp
+++ primeFactors.cpp
@@ -1,17 +1,34 @@
-//Program to find prime factors of given number in C++ language
+//Program to find prime factors of given positive number in C++ language
 #include <iostream>
 using namespace std;
 
 void primeFactors(int n)
 {
-  
+    int a = 2;
+
+    if(n == 1)
+        cout << n;
+
+    while(n > 1)
+    {
+        while(n % a == 0)
+        {
+            n /= a;
+            cout << a << "\t";
+        }
+        
+        a++;
+    }
+
+    if(n != 1)
+        cout << n;
 } 
   
 int main()
 {
-  int n = 42383493;
+    int n = 18;
   
-  primeFactors(n);
+    primeFactors(n);
   
-  return 0; 
+    return 0; 
 }
