bd458fbc02ea3c76de7dec0a900a4db8ec10bf48
ci3l4kM4sl4k <dubieltomasz@protonmail.com>
2022-09-11 12:50:53 +0200

    Create primeFactors.cpp

---
+++ primeFactors.cpp
@@ -0,0 +1,17 @@
+//Program to find prime factors of given number in C++ language
+#include <iostream>
+using namespace std;
+
+void primeFactors(int n)
+{
+  
+} 
+  
+int main()
+{
+  int n = 42383493;
+  
+  primeFactors(n);
+  
+  return 0; 
+}
