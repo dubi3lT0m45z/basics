be31a6276b19b0da51061a0256800dee3be70944
ci3l4kM4sl4k <dubieltomasz@protonmail.com>
2022-09-12 20:10:36 +0200

    Update greatestCommonDivisor.cpp

--- greatestCommonDivisor.cpp
+++ greatestCommonDivisor.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 using namespace std;
 
+////////////////// ready function
+void GCD(int a, int b)
+{
+    while(a != b)
+    {
+        if(a > b)
+            a -= b;
+        else
+            b -= a;
+    }
+    cout << "Greatest common divisor of given numbers is: " << a;
+}
+//////////////////
+
 int main()
 {
-  
-  return 0; 
+    int a = 423, b = 234;
+
+    GCD(a, b);
+
+    return 0;
 }
