ecf3b51a1cb3c19b5aa68f681ac053faa1a6d03c
ci3l4kM4sl4k <dubieltomasz@protonmail.com>
2022-09-13 14:45:19 +0200

    Update brownianMotion.cpp

--- brownianMotion.cpp
+++ brownianMotion.cpp
@@ -1,9 +1,25 @@
 //Algotithm for geometric Brownian motion in C++
 #include <iostream>
+#include <math.h>
 using namespace std;
+
+int brownianMotion(int n)
+{
+    int s;
+
+    while()
+
+    return s;
+}
 
 int main()
 {
-  
-  return 0;
+    int n, s;
+
+    cout << "How many steps? ";
+    cin >> n;
+
+    cout << "The motion vector equals: " << brownianMotion(n);
+
+    return 0;
 }
