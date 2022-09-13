73ce32465cbd4237f4ff25081fad7aa2966759cc
ci3l4kM4sl4k <dubieltomasz@protonmail.com>
2022-09-13 14:57:50 +0200

    Update brownianMotion.cpp

--- brownianMotion.cpp
+++ brownianMotion.cpp
@@ -1,20 +1,30 @@
 //Algotithm for geometric Brownian motion in C++
 #include <iostream>
+#include <stdlib.h>
 #include <math.h>
 using namespace std;
 
-int brownianMotion(int n)
+float brownianMotion(int n)
 {
-    int s;
+    int i = 0;
+    float s = 0, x = 0, y = 0, fi = 0;
+    srand(time(NULL));
 
-    while()
+    while(i <= n)
+    {
+        fi = ((float)rand()/RAND_MAX)*2*M_PI;
+        x += cos(fi);
+        y += sin(fi);
+        i++;
+    }
+    s = sqrt(pow(x,2)+pow(y,2));
 
     return s;
 }
 
 int main()
 {
-    int n, s;
+    int n;
 
     cout << "How many steps? ";
     cin >> n;
