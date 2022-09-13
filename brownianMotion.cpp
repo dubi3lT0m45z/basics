e53f87ce46eca91c184b4fc0d938498bed4698a1
ci3l4kM4sl4k <dubieltomasz@protonmail.com>
2022-09-13 15:04:41 +0200

    Update brownianMotion.cpp

--- brownianMotion.cpp
+++ brownianMotion.cpp
@@ -4,30 +4,32 @@
 #include <math.h>
 using namespace std;
 
+///////////////////////////ready function
 float brownianMotion(int n)
 {
     int i = 0;
     float s = 0, x = 0, y = 0, fi = 0;
-    srand(time(NULL));
+    srand(time(NULL)); //it makes so random numbers will always be random
 
     while(i <= n)
     {
-        fi = ((float)rand()/RAND_MAX)*2*M_PI;
-        x += cos(fi);
-        y += sin(fi);
+        fi = ((float)rand()/RAND_MAX)*2*M_PI; //direction
+        x += cos(fi); //distance on the x axis
+        y += sin(fi); //distance on the y axis
         i++;
     }
-    s = sqrt(pow(x,2)+pow(y,2));
+    s = sqrt(pow(x,2)+pow(y,2)); //final distance from the starting point(0,0)
 
     return s;
 }
+/////////////////////////////
 
 int main()
 {
     int n;
 
     cout << "How many steps? ";
-    cin >> n;
+    cin >> n; //number of steps
 
     cout << "The motion vector equals: " << brownianMotion(n);
 
