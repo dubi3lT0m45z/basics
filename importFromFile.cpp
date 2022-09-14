c7ef54ec1b5af1917aa094cde848ecf32d7ad7ad
ci3l4kM4sl4k <dubieltomasz@protonmail.com>
2022-09-14 16:39:18 +0200

    Update importFromFile.cpp

--- importFromFile.cpp
+++ importFromFile.cpp
@@ -2,9 +2,30 @@
 #include <fstream>
 using namespace std;
 
+void import(int arr[])
+{
+    fstream file;
+    int i = 0;
+
+    file.open("fileName.txt", ios::in);
+        while (!file.eof())
+        {
+            file >> arr[i];
+            i++;
+        }
+        
+    file.close();
+}
+
 int main()
 {
-  
-  
-  return 0;
+    int n = 20;
+    int arr[n];
+
+    import(arr);
+
+    for(int i = 0; i < n; i++)
+        cout << i+1 << ". " << arr[i] << "\n";
+
+    return 0;
 }
