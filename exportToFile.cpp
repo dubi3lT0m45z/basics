0f8f245858c34f117687b1a45e18c5a18f12b5e8
ci3l4kM4sl4k <dubieltomasz@protonmail.com>
2022-09-15 14:35:10 +0200

    Update exportToFile.cpp

--- exportToFile.cpp
+++ exportToFile.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
+
+void exportToFile(string fileName)
+{
+
+}
 
 int main()
 {
-  
-  
-  return 0;
+    string fileName, type;
+
+    cout << "How you want to call new file? ";
+    cin >> fileName;
+
+    do
+    {
+        cout << "What kind of numbers you want to be generated?\n";
+        cout << "N - natural\nW - whole\nZ - intefers" << endl;
+        cin >> type;
+    }
+    while(type != 'N' || type != 'W' || type != 'Z');
+
+    cout << type;
+
+    return 0;
 }
