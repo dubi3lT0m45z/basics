5abfe926be1624d0632acd8402f6de2d038e0114
ci3l4kM4sl4k <dubieltomasz@protonmail.com>
2022-09-15 14:46:14 +0200

    Update exportToFile.cpp

--- exportToFile.cpp
+++ exportToFile.cpp
@@ -9,20 +9,35 @@ void exportToFile(string fileName)
 
 int main()
 {
-    string fileName, type;
+    string fileName, q1, type;
 
     cout << "How you want to call new file? ";
     cin >> fileName;
 
     do
     {
-        cout << "What kind of numbers you want to be generated?\n";
-        cout << "N - natural\nW - whole\nZ - intefers" << endl;
-        cin >> type;
-    }
-    while(type != 'N' || type != 'W' || type != 'Z');
+        cout << "Do you want random numbers, or you want to write them?\n";
+        cout << "R - random\nW - write" << endl;
+        cin >> q1;
+    }while(q1 != "R" && q1 != "W");
+    
+    cout << "How many numbers do you want in file?\n";
+    cin >> fileName;
 
-    cout << type;
+    if(q1 == "W")
+    {
+
+    }
+    else
+    {
+        do
+        {
+            cout << "What kind of numbers you want to be generated?\n";
+            cout << "N - natural\nW - whole\nZ - intefers" << endl;
+            cin >> type;
+        }
+        while(type != "N" && type != "W" && type != "Z");
+    }
 
     return 0;
 }
