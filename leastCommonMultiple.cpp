4e8ff1972c6298dc536b5268bbcbc2488c2bc0d3
ci3l4kM4sl4k <dubieltomasz@protonmail.com>
2022-09-16 19:42:28 +0200

    Update leastCommonMultiple.cpp

--- leastCommonMultiple.cpp
+++ leastCommonMultiple.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
 using namespace std;
 
+void LCM(int x, int y)
+{
+    int lcm;
+
+    if(x > y)
+        lcm = x;
+    else
+        lcm = y;
+
+    while(lcm % x != 0 || lcm % y != 0)
+        lcm++;
+
+    cout << "Least common multiplication of given numbers is: " << lcm;
+}
+
 int main()
 {
-  
-  
-  return 0;
+    int a, b;
+
+    cout << "Enter first number: ";
+    cin >> a;
+
+    cout << "Enter secound number: ";
+    cin >> b;
+
+    LCM(a,b);
+
+    return 0;
 }
